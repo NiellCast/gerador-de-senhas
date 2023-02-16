000c2c0f96faf701b9240faeb33c14f48b25076c
Daniell Castelo Branco Ciriaco <danicbciriaco2@gmail.com>
2023-02-16 18:47:15 -0300

    Otimizando o código

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,39 @@ const botaoGerar = document.querySelector("#btn-gerador")
 const tamanhoEscolhido = document.querySelector("#tamanho-da-senha")
 const mostrarQuantidade = document.querySelector("#numero-do-range")
 const senhaGerada = document.querySelector("#senha-gerada")
+const letras = document.querySelector("#c-letras")
+const numeros = document.querySelector("#c-numeros")
+const especiais = document.querySelector("#c-especiais")
 
 
-function gerador(quantidade) {
-    let caracteres = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%¨&*()_/=-+*,.;?><][}{`^~"
+function gerador(quantidade, letras, numeros) {
+    const caracteres = {
+        letras: "abcdefghijklmnopqrstuvwxyz!@#$%¨&*()_/=-+*,.;?><][}{`^~1234567890",
+        numeros: "1234567890"
+    }
+    let caracteresPossiveis = []
     let senha = ''
 
+
+    switch (letras) {
+        case true:
+            caracteresPossiveis += caracteres.letras
+            break
+    }
+
+    switch (numeros) {
+        case true:
+            caracteresPossiveis += caracteres.numeros
+            break
+    }
+
+
     do {
-        senha += caracteres[Math.floor(Math.random() * caracteres.length)]
-    } while (senha.length < quantidade) 
+        let caracterGerado = caracteresPossiveis[Math.floor(Math.random() * caracteresPossiveis.length)]
+
+        senha += caracterGerado
+
+    } while (senha.length < quantidade)
 
     return senha
 }
@@ -25,11 +49,9 @@ tamanhoEscolhido.addEventListener('pointermove', () => {
 })
 
 botaoGerar.addEventListener('click', () => {
-    senhaGerada.innerHTML = gerador(tamanhoEscolhido.value)
-    senhaGerada.style.display = 'block'
-
-    tamanhoEscolhido.value = 4
-    mostrarQuantidade.innerHTML = 4
+    senhaGerada.innerHTML = gerador(tamanhoEscolhido.value, letras.checked, numeros.checked)
+    senhaGerada.style.display = 'flex'
+    senhaGerada.classList.add('linha-centralizada')
 })
 
 senhaGerada.addEventListener('click', () => {
